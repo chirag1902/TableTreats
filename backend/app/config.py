16c783a665b3c79d25c34a8adce4b6d60a424994
Chirag Goel <chirag.goel@rutgers.edu>
2025-10-03 14:38:26 -0400

    Changed Resturant db in config to Customer db

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-MONGO_DB = os.getenv("MONGO_DB", "restaurant_db")
+MONGO_DB = os.getenv("MONGO_DB", "customer_db")
 SESSION_EXPIRE_MINUTES = int(os.getenv("SESSION_EXPIRE_MINUTES", 60))
