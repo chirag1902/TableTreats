bde2dc7eea36af953be6ef50e787ebc752af7883
Chirag Goel <chirag.goel@rutgers.edu>
2025-12-07 13:13:22 -0500

    Added .env in customer app

--- frontend/customer-app/src/services/api.js
+++ frontend/customer-app/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000", // Your FastAPI backend URL
+  baseURL: process.env.REACT_APP_API_URL || "localhost:8000",
   headers: {
     "Content-Type": "application/json",
   },
